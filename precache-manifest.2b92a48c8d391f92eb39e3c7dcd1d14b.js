26f539606e0345ee4dba16cf391b5f14bd5aaa43
kindman <veldgon1996@gmail.com>
2020-07-07 18:33:13 +0300

    Updates

---
+++ precache-manifest.2b92a48c8d391f92eb39e3c7dcd1d14b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e098fbc7f772ee4ef1377ee88bab8ea",
+    "url": "/test-task/index.html"
+  },
+  {
+    "revision": "1dc9579e367dfb9e0cae",
+    "url": "/test-task/static/css/main.24fd92a2.chunk.css"
+  },
+  {
+    "revision": "ca685224e30824863983",
+    "url": "/test-task/static/js/2.da897054.chunk.js"
+  },
+  {
+    "revision": "6e900953f82e75bb7ccced89e6806918",
+    "url": "/test-task/static/js/2.da897054.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dc9579e367dfb9e0cae",
+    "url": "/test-task/static/js/main.5c971f23.chunk.js"
+  },
+  {
+    "revision": "79892429a743b8954c05",
+    "url": "/test-task/static/js/runtime-main.4af6997d.js"
+  }
+]);
